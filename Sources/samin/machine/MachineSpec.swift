3f940c57af6e534fa0b515b0d5f367f471b3c02e
Dale Anderson <dale.anderson@roadtrans.co.nz>
2020-08-29 22:28:31 +1200

    Add basic machine_spec path check.

--- Sources/samin/machine/MachineSpec.swift
+++ Sources/samin/machine/MachineSpec.swift
@@ -2,12 +2,34 @@
 // Created by swishy on 29/08/20.
 //
 
+import Foundation
 import FoundationXML
 
 class MachineSpec: XmlSaxBase {
 
+    // TODO make this pluggable so we can pull from NSBundle on iOS
+    func getMachineSpecPath() -> String {
+        let fileManager = FileManager()
+        if let home = ProcessInfo.processInfo.environment["HOME"] {
+            if(fileManager.fileExists(atPath: home + "/.amin/machine_spec.xml")) {
+                return home + "/.amin/machine_spec.xml"
+            }
+        }
+        if let envSpec = ProcessInfo.processInfo.environment["AMIN_MACHINE_SPEC"] {
+            if(fileManager.fileExists(atPath: envSpec)) {
+                return envSpec
+            }
+        }
+        return "/etc/machine_spec.xml"
+    }
+
     override func parserDidStartDocument(_ parser: XMLParser) {
         print("machine spec start doc")
+
+        let machineSpecUrl = URL(string: getMachineSpecPath())
+        print(machineSpecUrl)
+
+
     }
 
 }
