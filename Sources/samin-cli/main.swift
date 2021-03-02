75951693e12077b2bf56bc5d7ac0a48dc4e1aac1
Dale Anderson <swishyTM@gmail.com>
2021-03-03 11:54:31 +1300

    Pushing test code to track down cause of stream delegate not firing

--- Sources/samin-cli/main.swift
+++ Sources/samin-cli/main.swift
@@ -2,15 +2,30 @@ import Foundation
 import samin
 
 class SaminCli: NSObject, StreamDelegate {
-    func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
+    func stream(aStream: Stream, handle eventCode: Stream.Event) {
         print("Event!")
+        print(eventCode)
     }
 
     func main() {
         print("SAmin cause well.. 42")
+
+
         let amin = Samin()
         let profile = "<amin:command name='mkdir' xmlns:amin='http://projectamin.org/ns/'>\n\t<amin:flag name='m'>0755</amin:flag>\n\t<amin:param name=\"target\">/tmp/test_ashell</amin:param>\n</amin:command>"
         let data = profile.data(using: .utf8)
+
+        // Testing delegate
+
+        let testStream = OutputStream.toMemory()
+        testStream.delegate = self
+        testStream.open()
+        let xmlByteArray = [UInt8](profile.utf8)
+        testStream.write(xmlByteArray, maxLength: xmlByteArray.count)
+        testStream.close()
+
+        // Testing delegate
+
         let inputStream = InputStream(data: data!)
         let outputstream = OutputStream.toMemory()
         inputStream.schedule(in: RunLoop.main, forMode: RunLoop.Mode.default)
